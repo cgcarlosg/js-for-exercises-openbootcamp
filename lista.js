d9785254979a88cf9752d3845626787d764265b1
Carlos Albenio Gutierrez Monroy <carlos.gutierrez01@globant.com>
2022-07-08 17:40:25 -0500

    Lista js openbootcamp corregido

---
+++ lista.js
@@ -0,0 +1,13 @@
+var nombre = "Carlos Gutierrez";
+var edad = 43;
+var desarrollador=true;
+var nacimiento = new Date("february 09 1978");
+var libro = {
+    titulo:"Volar sobre el pantano",
+    autor: "Carlos Cuahutemoc Blanco",
+    fecha: "09-Abr-1995",
+    url: "https://mercadolibre.com.co"};
+
+var datos = [nombre, edad, desarrollador, nacimiento, libro];
+
+console.log(datos);

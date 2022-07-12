6f41183e280cff3347643f22d719420e2d65360a
Carlos Albenio Gutierrez Monroy <carlos.gutierrez01@globant.com>
2022-07-12 10:57:24 -0500

    ejercicio de Set

---
+++ conjuntos.js
@@ -0,0 +1,10 @@
+// Un nuevo Set con los nombres de tu familia
+const familia = ['Carlos', 'Diana', 'Anamaria', 'Andres', 'Juliana']
+const miSet = new Set(familia);
+
+// Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+miSet.add('Carlos Gutierrez');
+
+// Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+miSet.add('Javascript');
+console.log(miSet);
